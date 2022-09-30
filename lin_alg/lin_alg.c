24a6f3a6ac726ec488b4fc76986305d2fed6cf63
fred144 <fgarcia4@umd.edu>
2022-09-30 19:53:59 -0400

    stratign hw 2

---
+++ lin_alg/lin_alg.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <math.h>
+#include "./gaussj.c"
+#include "./nr.h"
+#include "./nrutil.h" 
+
+int main(){
+    float **a = matrix(1,3,1,3); 
+    float **b = matrix(1,3,1,1);
+    float *w = vector(1,3); 
+
+    
+    a[1][1] = -3; 
+    a[1][2] = 2;
+    a[1][3] = -5;
+    a[2][1] = 2; 
+    a[2][2] = -3;
+    a[2][3] = 4;
+    a[3][1] = 1;
+    a[3][2] = 1; 
+    a[3][3] = 1; 
+
+    b[1][1] = -14;
+    b[2][1] = 10;
+    b[3][1] = 4.0; 
+    svdcmp(a, 3, 1, w, b);
+    // gaussj(a, 3, b, 1);
+    printf("%g \n", b[1][1]);
+    printf("%g \n ", b[1][2]);
+    printf("%g \n", b[1][3]);
+    
+    printf("%g \n", w[1][3]);
+
+    // single variable decomposition
+
+}
---
+++ lin_alg/lin_alg_notes.md
@@ -0,0 +1,11 @@
+# Numerical Linear Algebra
+> https://www.astro.umd.edu/~ricotti/NEWWEB/teaching/ASTR415/pres06.pdf
+
+
+$
+a_{11}x_1+
+$
+## compile
+```terminal
+$ cc - o Ex1_gauss gaussh.c svdcmp.c pythag.c nrutil.c Ex1_gauss.c -lm 
+```
