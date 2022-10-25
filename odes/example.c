3ffdd056d616210e74cc3c8526c3e114676309d5
fred144 <fgarcia4@umd.edu>
2022-10-25 15:15:48 -0400

    notes

---
+++ odes/example.c
@@ -0,0 +1,15 @@
+#include <math.h>
+#include <stdio.h>
+
+float newtons(float distance){
+    float G = 6.67 * 1e-11
+    float M_earth = 
+
+}
+
+int main(){
+    float v_0 = 1; // km/s
+    float step = 0.0;
+    // https://www.astro.umd.edu/~ricotti/NEWWEB/teaching/ASTR415/pres15.pdf
+    // float v_n_plus_half = v;
+}
